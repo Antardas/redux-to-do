19445d124db52dc57ea28215dac5cc6252c62e14
Antardas <antardas2334@gmail.com>
2021-12-14 20:57:37 +0600

    Uninstall Tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className='te'>This is Header</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="bg-cyan-500 shadow-lg shadow-cyan-500/50 text-base	">Subscribe</button>
     </div>
   );
 }
